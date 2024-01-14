0813261803f0f07d14a9f4f28d9102ec940d3a1d
Nghi Nguyen <quangnghiams@gmail.com>
2024-01-14 02:57:01 -0500

    Remove debug lines

--- spin-up-script.sh
+++ spin-up-script.sh
@@ -2,7 +2,7 @@
 
 # Just a basic script to spin up listening servers on different ports
 PORTS=$(seq 40000 40050)
-noob=1
+
 export PYTHONPATH=src/main/py
 
 for port in ${PORTS}
@@ -10,8 +10,6 @@ do
   echo "Starting on port $port"
   python3 -m sample_module.udp_server ${port} &
 done
-
-echo $noob
 
 wait
 
