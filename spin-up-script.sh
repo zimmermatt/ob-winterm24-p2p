df120c3f92a7f5163e61fc59b3ddf1437b8fec13
Nghi Nguyen <quangnghiams@gmail.com>
2024-01-14 02:56:00 -0500

    Fix loop bug, now can spin up udp servers on multiple ports

--- spin-up-script.sh
+++ spin-up-script.sh
@@ -1,15 +1,18 @@
 #!/bin/sh
 
 # Just a basic script to spin up listening servers on different ports
-PORTS=$(seq 40000 40002)
-
+PORTS=$(seq 40000 40050)
+noob=1
 export PYTHONPATH=src/main/py
 
-for port in "${PORTS}"
+for port in ${PORTS}
 do
-  echo $port
-  python3 -m sample_module.udp_server ${port}
-
+  echo "Starting on port $port"
+  python3 -m sample_module.udp_server ${port} &
 done
+
+echo $noob
+
+wait
 
 
