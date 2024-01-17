610c2f458b2bbc09d1c05f2300f6f2b07e03a23e
Nghi Nguyen <quangnghiams@gmail.com>
2024-01-17 08:13:25 -0500

    Iterate through the ports and create a port address list

--- devops/deploy/spin-down-script.sh
+++ devops/deploy/spin-down-script.sh
@@ -2,3 +2,12 @@
 
 # I think best to keep a list of ports open somewhere in this devops directory to pull from
 # For now let's just keep it simple with the defaults
+PORTS=$(seq 50000 50050)
+IP=127.0.0.1
+
+PORTLIST=$()
+for port in ${PORTS}
+do
+	temp=$IP:$port
+	echo $temp
+done
