6be4b1caee6bd4dd1e4aba4ac085cbe38de4e6ec
Nghi Nguyen <quangnghiams@gmail.com>
2024-01-18 14:02:56 -0500

    Finish implementing spin down script spinning down based on port_list.txt

--- devops/deploy/spin-down-script.sh
+++ devops/deploy/spin-down-script.sh
@@ -1,13 +1,19 @@
-#!/bin/sh
+#!/bin/bash
 
-# I think best to keep a list of ports open somewhere in this devops directory to pull from
-# For now let's just keep it simple with the defaults
-PORTS=$(seq 50000 50050)
+# Now fully implemented spinning down through the port_list.txt
 IP=127.0.0.1
+declare -a PORTS=()
+while IFS= read -r line
+do
+	PORTS+=("$line")
+done < "port_list.txt"
 
 # Iterate through the list, killing each controlling process while looping
-for port in ${PORTS}
+for port in ${PORTS[*]}
 do
+	echo "Killing processes at $IP:$port"
 	temp=$IP:$port
 	kill -15 $(netstat -anp | grep $temp | awk '{print $6}' | awk -F/ '{print $1}')
 done
+
+echo -n > "port_list.txt"
