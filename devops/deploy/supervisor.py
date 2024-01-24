bb5f9a76917df6b2caab23adf231bb988ace3f48
Nghi Nguyen <quangnghiams@gmail.com>
2024-01-24 15:54:04 -0500

    Add recover function

--- devops/deploy/supervisor.py
+++ devops/deploy/supervisor.py
@@ -1,11 +1,14 @@
 # Importing
 import asyncio
+import subprocess
 
 # Placeholder for function to recover nodes recover_node(host, port)
-
+def recover_node(node_number, host, port):
+    key_file = "node" + str(node_number)
+    subprocess.run(['python3', '-m', 'peer.peer', port, key_file, host, '&')
 
 # Function to check if a node is alive
-async def ping_node(host, port):
+async def ping_node(node_number, host, port):
     try:
         reader, writer = await asyncio.open_connection(host, port)
         print(f"Alive node at {host}:{port}")
@@ -14,6 +17,7 @@ async def ping_node(host, port):
     except (OSError, asyncio.TimeoutError):
         print(f"Node at {host}:{port} is not responding")
         # Recover the node using recover_node(host, port) function
+        recover_node(node_number, host, port)
 
 # Main Function
 async def main():
@@ -26,7 +30,7 @@ async def main():
 
     while True:
         for i in range(len(peer_list)):
-            await ping_node("127.0.0.1", peer_list[i][1])
+            await ping_node(i + 1, "127.0.0.1", peer_list[i][1])
         await asyncio.sleep(5)
 
 if __name__ == "__main__":
