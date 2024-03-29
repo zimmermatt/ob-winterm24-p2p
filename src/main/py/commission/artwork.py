23c85594350c26feec89a9c0721859d5751c63d8
Yen Mai <110384864+yen-mai-codes@users.noreply.github.com>
2024-03-29 18:17:26 -0400

    added get_palette for Peer class, which a peer can call to generate their own palette based on existing constraints. Adheres to Kademlia distance notion.

--- src/main/py/commission/artwork.py
+++ src/main/py/commission/artwork.py
@@ -14,7 +14,7 @@ from peer.ledger import Ledger
 import utils
 
 Pixel = namedtuple("Pixel", ["coordinates", "color"])
-Pixel.__annotations__ = {"coordiantes": Coordinates, "color": Color}
+Pixel.__annotations__ = {"coordinates": Coordinates, "color": Color}
 Constraint = namedtuple("Constraint", ["color", "line_type"])
 Constraint.__annotations__ = {"color": Color, "line_type": str}
 
