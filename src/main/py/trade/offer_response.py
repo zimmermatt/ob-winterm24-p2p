be82f2513d3e0329c1a12c7d10796d27c4236000
Olivia Brown <99828822+oliviab598@users.noreply.github.com>
2024-03-30 16:24:37 -0400

    Test and refactor classes for trading art (#42)
    Created tests for the `Peer` class methods for trading art and altered
    classes accordingly and 2. Refactored the `peer_test.py` file to remove
    redundant code.
    
    1. In the `asyncio.get_event_loop().call_later()` block, changed
    `wait_time` to `wait_time.total_seconds()`. When testing, I got a
    `TypeError` with the `timedelta` object, so I needed to convert
    `wait_time` to seconds.
    
    2. Removed redundant code by adding `self.peer.logger`, `self.trade_key`
    and `self.offer_response` to `setUp()` in `peer_test` and refactored the
    file from there.
    
    3. Modified `artwork_ledger_key` to `artwork_id` in the
    `offer_announcement` module and `artwork_ledger_id` to `artwork_id` in
    the `offer_response` module for clarity and consistency across classes.
    
    ---------
    
    Co-authored-by: Olivia Brown <oliviabrown@MacBook-Pro-4.localdomain>
    Co-authored-by: Olivia Brown <oliviabrown@MacBook-Pro-4.local>

--- src/main/py/trade/offer_response.py
+++ src/main/py/trade/offer_response.py
@@ -14,7 +14,7 @@ class OfferResponse:
     def __init__(
         self,
         trade_id: str,
-        artwork_ledger_id: str,
+        artwork_id: str,
         originator_public_key: str = "",
     ):
         """
@@ -22,23 +22,26 @@ class OfferResponse:
         - trade_id (str): The trade id to respond to.
         """
         self.trade_id = trade_id
-        self.offer_ledger_id = artwork_ledger_id
+        self.artwork_id = artwork_id
         self.originator_public_key = originator_public_key
 
     def get_trade_id(self):
         """
         Returns the trade id to respond to.
         """
+
         return self.trade_id
 
     def get_originator_public_key(self):
         """
         Returns the public key of the originator of the trade response.
         """
+
         return self.originator_public_key
 
-    def get_offer_ledger_id(self):
+    def get_artwork_id(self):
         """
-        Returns the offer ledger id to respond to.
+        Returns the artwork ledger key to respond to.
         """
-        return self.offer_ledger_id
+
+        return self.artwork_id
